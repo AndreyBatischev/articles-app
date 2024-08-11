382ac0a494d9bfb9e1108b073501ba9bdfca5919
Andrey B <a.e.batischev@gmail.com>
2024-08-11 10:56:27 +0300

    add redis

--- src/modules/articles/articles.module.ts
+++ src/modules/articles/articles.module.ts
@@ -3,9 +3,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticlesService } from './articles.service';
 import { Articles } from './entities/articles.entity';
 import { ArticlesController } from './articles.controller';
+import * as redisStore from 'cache-manager-redis-store';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Articles])],
+  imports: [
+    TypeOrmModule.forFeature([Articles]),
+    CacheModule.register({
+      useFactory: (configService: ConfigService) => ({
+        store: typeof redisStore,
+        host: configService.get<string>('REDIS_DB_HOST'),
+        port: configService.get<number>('REDIS_PORT'),
+        ttl: configService.get<number>('REDIS_TTL'),
+      }),
+    }),
+  ],
   providers: [ArticlesService],
   controllers: [ArticlesController],
   exports: [ArticlesService],
--- src/modules/articles/articles.service.ts
+++ src/modules/articles/articles.service.ts
@@ -1,19 +1,33 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Between, FindManyOptions, Like, Repository } from 'typeorm';
 import { Articles } from './entities/articles.entity';
 import { CreateArticleDto } from './dto/create-article-dto';
 import { UpdateArticleDto } from './dto/update-article-dto';
+import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
+import { stringify } from 'querystring';
 
 @Injectable()
 export class ArticlesService {
   constructor(
     @InjectRepository(Articles)
     private articlesRepository: Repository<Articles>,
-  ) { }
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
-  async findAll(query: { page?: number; limit?: number; author?: string; publicationDate?: string }): Promise<Articles[]> {
+  async findAll(query: {
+    page?: number;
+    limit?: number;
+    author?: string;
+    publicationDate?: string;
+  }): Promise<Articles[]> {
     const { page = 1, limit = 10, author, publicationDate } = query;
+
+    const cacheKey = `articles_${stringify(query)}`;
+    const cachedArticles = await this.cacheManager.get<Articles[]>(cacheKey);
+    if (cachedArticles) {
+      return cachedArticles;
+    }
 
     const options: FindManyOptions<Articles> = {
       skip: (page - 1) * limit,
@@ -34,30 +48,55 @@ export class ArticlesService {
       options.where['publicatedDate'] = Between(startOfDay, endOfDay);
     }
 
-    return this.articlesRepository.find(options);
+    const articles = await this.articlesRepository.find(options);
+    await this.cacheManager.set(cacheKey, articles, 60);
+
+    return articles;
   }
 
   async findOne(id: string): Promise<Articles> {
+    const cacheKey = `article_${id}`;
+    const cachedArticle = await this.cacheManager.get<Articles>(cacheKey);
+    if (cachedArticle) {
+      return cachedArticle;
+    }
     const article = await this.articlesRepository.findOne({ where: { id } });
+
     if (!article) {
       throw new NotFoundException(`Article with ID ${id} not found`);
     }
+    await this.cacheManager.set(cacheKey, article, 60);
+
     return article;
   }
 
   async create(createArticleDto: CreateArticleDto): Promise<Articles> {
     const article = this.articlesRepository.create(createArticleDto);
-    return this.articlesRepository.save(article);
+    const savedArticle = await this.articlesRepository.save(article);
+    await this.cacheManager.del('all_articles');
+    return savedArticle;
   }
 
-  async update(id: string, updateArticleDto: UpdateArticleDto): Promise<Articles> {
+  async update(
+    id: string,
+    updateArticleDto: UpdateArticleDto,
+  ): Promise<Articles> {
     const article = await this.findOne(id);
     Object.assign(article, updateArticleDto);
-    return this.articlesRepository.save(article);
+    const updatedArticles = await this.articlesRepository.save(article);
+    await this.cacheManager.del('all_articles');
+    await this.cacheManager.del(`article_${id}`);
+    return updatedArticles;
   }
 
-  async remove(id: string): Promise<void> {
+  async remove(id: string): Promise<string> {
     const article = await this.findOne(id);
+    if (!article) {
+      throw new NotFoundException(`Article with ID ${id} not found`);
+    }
     await this.articlesRepository.remove(article);
+    await this.cacheManager.del('all_articles');
+    await this.cacheManager.del(`article_${id}`);
+    return `Article with ID ${id} success delete`;
   }
 }
